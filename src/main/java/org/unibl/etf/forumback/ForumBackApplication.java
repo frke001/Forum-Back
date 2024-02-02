a867a9f1c2129d3acee96c848f4cf79dfcc9231d
NDj01 <nemanjadj102@gmail.com>
2024-02-02 13:54:48 +0100

    Initial commit

---
+++ src/main/java/org/unibl/etf/forumback/ForumBackApplication.java
@@ -0,0 +1,13 @@
+package org.unibl.etf.forumback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ForumBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ForumBackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/unibl/etf/forumback/ForumBackApplicationTests.java
@@ -0,0 +1,13 @@
+package org.unibl.etf.forumback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ForumBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
